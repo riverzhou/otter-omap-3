9f1c55e319717a5efd9a04d54200f4b7328b7cfe
River Zhou <riverzhou2000@gmail.com>
2012-11-11 20:57:10 +0800

    revert arch/arm/mach-omap2/ to T1

--- arch/arm/mach-omap2/board-4430kc1-power.c
+++ arch/arm/mach-omap2/board-4430kc1-power.c
@@ -197,6 +197,11 @@ static struct twl4030_madc_platform_data sdp4430_gpadc_data = {
 	.irq_line = -1,
 };
 
+#define SUMMIT_STAT 31
+static struct twl6030_qcharger_platform_data kc1_charger_data={
+        .interrupt_pin = OMAP4_CHARGER_IRQ,
+};
+
 static struct regulator_init_data sdp4430_clk32kg = {
 	.constraints = {
 		.valid_modes_mask	= REGULATOR_MODE_NORMAL,
